c0e674d85d847c3f1ac9c804ec1685ca47fc0a31
Buglot <65818720+buglot@users.noreply.github.com>
2022-06-22 02:03:01 +0700

    v3

--- imagsShow.py
+++ imagsShow.py
@@ -3,7 +3,7 @@ import imp
 from re import T
 from tkinter.tix import Tree
 from typing_extensions import Self
-from PyQt6.QtWidgets import QWidget,QLabel,QHBoxLayout,QVBoxLayout,QSizePolicy,QScrollArea
+from PyQt6.QtWidgets import QWidget,QGraphicsView,QHBoxLayout,QVBoxLayout,QSizePolicy,QScrollArea,QPushButton,QGraphicsScene, QGraphicsPixmapItem
 from PyQt6.QtGui import QPixmap
 from PyQt6.QtCore import QSize,Qt
 import numpy
@@ -11,13 +11,15 @@ class showIMG(QWidget):
     def __init__(self,filenames):
         super().__init__()
         self.setGeometry(0,0,600,600)
-        self.__Scroll = QScrollArea()
         self.__pre_img = QPixmap(filenames)
-        self.__img=QLabel()
-        self.__Scroll.setWidget(self.__img)
-        self.__Scroll.setWidgetResizable(True)
+        self.sase=QGraphicsScene()
+        self.mainPhoto=QGraphicsView(self.sase)
+        self.__img=QGraphicsPixmapItem(self.__pre_img)
+        self.sase.addItem(self.__img)
+
         self.__la = QVBoxLayout()
-        self.__img.setPixmap(self.__pre_img.scaled(600-5,600-5,Qt.AspectRatioMode.KeepAspectRatio,Qt.TransformationMode.SmoothTransformation))
+        self.__buttons = QPushButton("Full")
+        self.__buttons.clicked.connect(self.full_click)
         if len(filenames)<=10:
             self.__strs=filenames
         else:
@@ -26,9 +28,34 @@ class showIMG(QWidget):
         self.__la.addLayout(self.__layout)
         self.setWindowTitle("Show img"+self.__strs)
         self.setLayout(self.__la)
-        self.__layout.addWidget(self.__Scroll)
-        self.resizeEvent = self.__as
-        self.__img.setScaledContents(1)
-        self.__img.setSizePolicy(QSizePolicy.Policy.Fixed,QSizePolicy.Policy.Fixed)
-    def __as(self,e):
-        print(e.oldSize())
+        self.__layout.addWidget(self.mainPhoto)
+        self.__la.addWidget(self.__buttons)
+        self.resizeEvent = self.__autoREsize
+        self.mainPhoto.fitInView(self.sase.sceneRect(),Qt.AspectRatioMode.KeepAspectRatio)
+        self.mainPhoto.moveEvent =self.a
+        self.wheelEvent =self.WheelEvents
+        self.mainPhoto.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAsNeeded)
+        
+    def a(self,s):
+        self.pointMouse =s
+        print(s.pos())
+    def WheelEvents(self,event):
+        
+        zoomInFactor = 1.25
+        zoomOutFactor = 1 / zoomInFactor
+        self.mainPhoto.setTransformationAnchor(QGraphicsView.ViewportAnchor.NoAnchor)
+        self.mainPhoto.setResizeAnchor(QGraphicsView.ViewportAnchor.NoAnchor)
+        oldPos = self.mainPhoto.mapToScene(self.pointMouse.pos())
+        if event.angleDelta().y() > 0:
+            zoomFactor = zoomInFactor
+        else:
+            zoomFactor = zoomOutFactor
+        self.mainPhoto.scale(zoomFactor, zoomFactor)
+        newPos = self.mainPhoto.mapToScene(self.pointMouse.pos())
+        delta = newPos - oldPos
+        self.mainPhoto.translate(delta.x(), delta.y())
+
+    def __autoREsize(self,e):
+        self.mainPhoto.fitInView(self.sase.sceneRect(),Qt.AspectRatioMode.KeepAspectRatio)
+    def full_click(self):
+        self.mainPhoto.fitInView(self.sase.sceneRect(),Qt.AspectRatioMode.KeepAspectRatioByExpanding)
